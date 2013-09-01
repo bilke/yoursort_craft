25d3f8529eff80e0cd160c3ad31cf3ab802dc5d2
Lars Bilke <lars.bilke@ufz.de>
2013-09-01 22:35:45 +0200

    Added multi-env config.

--- craft/config/general.php
+++ craft/config/general.php
@@ -7,6 +7,99 @@
  * You can see a list of the default settings in craft/app/etc/config/defaults/general.php
  */
 
-return array(
+// Ensure our urls have the right scheme
+define('URI_SCHEME',  ( isset($_SERVER['HTTPS'] ) ) ? "https://" : "http://" );
+
+// The site url
+define('SITE_URL',    URI_SCHEME . $_SERVER['SERVER_NAME'] . '/');
+
+// The site basepath
+define('BASEPATH', 	  realpath(dirname(__FILE__) . '/../') . '/');
+
+$customConfig = array(
+
+  // ------------------------------------------------------------
+	// Environment: All
+  // ------------------------------------------------------------
+  '*' => array(
+
+    // Environmental variables
+    // We can use these variables in the URL and Path settings
+    // within the Craft Control Panel. For example:
+    //    siteUrl   can be references as {siteUrl}
+    //    basePath  can be references as {basePath}
+  	'environmentVariables' => array(
+  	  'basePath' => BASEPATH,
+      'siteUrl'  => 'http://dev.bilke.info'
+  	),
+
+    // Triggers
+    'cpTrigger' 		  => 'admin',
+    'resourceTrigger' => 'resources',
+    'actionTrigger' 	=> 'actions',
+    'pageTrigger' 		=> 'p',
+
+    // Member login info duration
+    // http://www.php.net/manual/en/dateinterval.construct.php
+    'userSessionDuration'           => 'P1M',
+    'rememberedUserSessionDuration' => 'P1M',
+    'rememberUsernameDuration'      => 'P1M',
+
+    // User account related paths
+    'loginPath'              => 'login',
+    'logoutPath'             => 'logout',
+    'setPasswordPath'        => 'setpassword',
+    'setPasswordSuccessPath' => '',
+    'activateAccountPath'    => 'activate',
+    'activateFailurePath'    => '',
+
+    // Manage our routes in the craft/config/routes.php file
+    // 'siteRoutesSource'   => 'file',
+
+    'mcsubApikey' => 'fb3a2615f8b6fa4eeb186d909873d6c2-us1',
+	'mcsubListId' => 'de170e4eab',
+
+	),
+
+  // ------------------------------------------------------------
+  // Environment: Development
+  // ------------------------------------------------------------
+	'dev' => array(
+
+    // Give us more useful error messages
+    'devMode' => true,
+
+    // Route ALL of the emails that Craft
+    // sends to a single email address.
+    'testToEmailAddress'  => '',
+
+    'translationDebugOutput'      => false,
+    'useCompressedJs'             => true,
+    'cacheDuration'               => 'P1D',
+    'cooldownDuration'            => 'PT5M',
+    'maxInvalidLogins'            => 5,
+    'invalidLoginWindowDuration'  => 'PT1H',
+    'phpMaxMemoryLimit'           => '256M',
+
+    // Member login info duration
+    // http://www.php.net/manual/en/dateinterval.construct.php
+    'userSessionDuration'           => 'P101Y',
+    'rememberedUserSessionDuration' => 'P101Y',
+    'rememberUsernameDuration'      => 'P101Y',
+
+    'environmentVariables' => array(
+      'siteUrl' => 'http://yoursortcraft.dev/'
+    )
+
+  )
 
 );
+
+// If a local config file exists, merge any local config settings
+if (is_array($customLocalConfig = @include(CRAFT_CONFIG_PATH . 'local/general.php')))
+{
+  $customGlobalConfig = array_merge($customConfig['*'], $customLocalConfig);
+  $customConfig['*'] = $customGlobalConfig;
+}
+
+return $customConfig;
